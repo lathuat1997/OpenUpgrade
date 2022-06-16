4140a9dd09d9c6a1aa070e919794289686352461
lathuat1997 <lathuat1997@gmail.com>
2022-06-16 13:35:45 +0700

    [MIG]fleet*: move data from fleet_vehicle_cost to fleet_vehicle_log_services

--- openupgrade_scripts/scripts/fleet/14.0.0.1/post-migration.py
+++ openupgrade_scripts/scripts/fleet/14.0.0.1/post-migration.py
@@ -3,6 +3,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
 
+from odoo.tools.translate import _
+
 
 def fill_fleet_vehicle_log_contract_fields(env):
     openupgrade.logged_query(
@@ -68,6 +70,67 @@ def recompute_fleet_vehicle_log_contract_name(env):
     contracts._compute_contract_name()
 
 
+def _generate_fuel_service_type(env):
+    fuel_service_type = env['fleet.service.type'].create({
+        'name': _('Refueling'),
+        'category': 'service'
+        })
+    return fuel_service_type
+
+def _generate_other_service_type(env):
+    other_service_type = env['fleet.service.type'].create({
+        'name': _('Other'),
+        'category': 'service'
+        })
+    return other_service_type
+
+def _move_data_from_cost_to_service(env, orther_service_type):
+    openupgrade.logged_query(
+        env.cr,"""
+        INSERT INTO fleet_vehicle_log_services (cost_id, vehicle_id, amount, date, description, create_uid, create_date,
+        write_uid, write_date, active, service_type_id, odometer_id, company_id)
+        SELECT id, vehicle_id, amount, date, description, create_uid, create_date, write_uid, write_date, True, COALESCE(cost_subtype_id, {0}),
+        odometer_id, company_id
+        FROM fleet_vehicle_cost c
+        WHERE c.cost_type = 'other';
+        """ .format(orther_service_type.id)
+    )
+
+def _move_data_from_fuel_to_service(env, fuel_service_type):
+    openupgrade.logged_query(
+        env.cr,"""
+        INSERT INTO fleet_vehicle_log_services (cost_id, vehicle_id, amount, date, description, create_uid, create_date,
+            write_uid, write_date, active, service_type_id, odometer_id, company_id)
+        SELECT c.id, c.vehicle_id, c.amount, c.date, c.description, c.create_uid, c.create_date, c.write_uid, c.write_date, True, COALESCE(c.cost_subtype_id, {0}),
+         c.odometer_id, c.company_id
+        FROM fleet_vehicle_cost c join fleet_vehicle_log_fuel f ON c.id = f.cost_id
+        """.format(fuel_service_type.id)
+    )
+
+def _check_vehicle_cost_have_no_type(env):
+        env.cr.execute("""
+        SELECT *
+        FROM fleet_vehicle_cost c
+        WHERE c.cost_type = 'other' AND c.cost_subtype_id IS NULL;
+        """
+        )
+        costs = env.cr.fetchone()
+        if costs:
+            return True
+        return False
+
+def _check_vehicle_fuel_cost_have_no_type(env):
+        env.cr.execute("""
+        SELECT *
+        FROM fleet_vehicle_cost c join fleet_vehicle_log_fuel f ON c.id = f.cost_id
+        WHERE c.cost_subtype_id IS NULL;
+        """
+        )
+        costs = env.cr.fetchone()
+        if costs:
+            return True
+        return False
+
 @openupgrade.migrate()
 def migrate(env, version):
     fill_fleet_vehicle_log_contract_fields(env)
@@ -87,3 +150,13 @@ def migrate(env, version):
         ],
     )
     recompute_fleet_vehicle_log_contract_name(env)
+    
+    # Move data from fleet_vehicle_cost and fleet_vehicle_log_fuel to fleet_vehicle_log_services
+    other_service_type = None
+    fuel_service_type = None
+    if _check_vehicle_fuel_cost_have_no_type(env):
+        fuel_service_type = _generate_fuel_service_type(env)
+    if _check_vehicle_cost_have_no_type(env):
+        other_service_type = _generate_other_service_type(env)
+    _move_data_from_cost_to_service(env, other_service_type)
+    _move_data_from_fuel_to_service(env, fuel_service_type)
